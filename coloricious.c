4164093aff07b8067cc66535cbf31a88a5e66d50
Christopher Ptak <cptak@comcast.net>
2022-03-12 01:14:37 -0500

    Added 8-bit color mode

--- coloricious.c
+++ coloricious.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+static float truncate(float);
+
 static void color8bit(float, float, float);
 static void color24bit(float, float, float);
 static void uncolor(void);
@@ -21,7 +23,7 @@ int main(void)
         float g = (float) rand() / INT_MAX;
         float b = (float) rand() / INT_MAX;
 
-        color24bit(r, g, b);
+        color8bit(r, g, b);
         putchar(c);
     }
 
@@ -30,11 +32,25 @@ int main(void)
     return EXIT_SUCCESS;
 }
 
+static float truncate(float f)
+{
+    return fmax(0.0, fmin(f, 1.0));
+}
+
+static void color8bit(float r, float g, float b)
+{
+    int rc = round(6 * truncate(r));
+    int gc = round(6 * truncate(g));
+    int bc = round(6 * truncate(b));
+    int cc = (36 * rc) + (6 * gc) + bc + 16;
+    printf("\e[38;5;%dm", cc);
+}
+
 static void color24bit(float r, float g, float b)
 {
-    int rc = round(255 * fmax(0.0, fmin(r, 1.0)));
-    int gc = round(255 * fmax(0.0, fmin(g, 1.0)));
-    int bc = round(255 * fmax(0.0, fmin(b, 1.0)));
+    int rc = round(255 * truncate(r));
+    int gc = round(255 * truncate(g));
+    int bc = round(255 * truncate(b));
     printf("\e[38;2;%d;%d;%dm", rc, gc, bc);
 }
 
