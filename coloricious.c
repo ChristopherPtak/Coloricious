c254f19cfedf61398325e5a881d93a7dbac35016
Christopher Ptak <cptak@comcast.net>
2022-03-12 02:53:51 -0500

    Added rainbow colorization by HSV

--- coloricious.c
+++ coloricious.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <limits.h>
 #include <math.h>
 #include <stdio.h>
@@ -15,6 +16,7 @@ struct color
 };
 
 struct color randomcolor(void);
+struct color fromhsv(float, float, float);
 
 static void color8bit(struct color);
 static void color24bit(struct color);
@@ -23,13 +25,21 @@ static void uncolor(void);
 int main(void)
 {
     struct color rgb;
+    float hue = 0.0;
     int c;
 
     srand(time(NULL));
 
     while ((c = getchar()) != -1) {
-        color24bit(randomcolor());
+
+        color24bit(fromhsv(hue, 1, 1));
         putchar(c);
+
+        if (hue < 1.0) {
+            hue += 0.01;
+        } else {
+            hue = 0.0;
+        }
     }
 
     uncolor();
@@ -48,6 +58,48 @@ struct color randomcolor(void)
     rgb.r = (float) rand() / INT_MAX;
     rgb.g = (float) rand() / INT_MAX;
     rgb.b = (float) rand() / INT_MAX;
+    return rgb;
+}
+
+struct color fromhsv(float h, float s, float v)
+{
+    h = truncate(h);
+    s = truncate(s);
+    v = truncate(v);
+
+    float chroma = v * s;
+    float x = chroma * (1 - fabs(fmod(h * 6, 2) - 1));
+    float m = v - chroma;
+
+    float r1 = 0.0;
+    float g1 = 0.0;
+    float b1 = 0.0;
+
+    if (h < 0.166666) {
+        r1 = chroma;
+        g1 = x;
+    } else if (h < 0.333333) {
+        r1 = x;
+        g1 = chroma;
+    } else if (h < 0.500000) {
+        g1 = chroma;
+        b1 = x;
+    } else if (h < 0.666666) {
+        g1 = x;
+        b1 = chroma;
+    } else if (h < 0.833334) {
+        r1 = x;
+        b1 = chroma;
+    } else {
+        r1 = chroma;
+        b1 = x;
+    }
+
+    struct color rgb;
+    rgb.r = r1 + m;
+    rgb.g = g1 + m;
+    rgb.b = b1 + m;
+
     return rgb;
 }
 
