7eba2adad21eaddae63a419ff52f95e867554a67
Christopher Ptak <cptak@comcast.net>
2022-03-12 01:22:37 -0500

    Refactored and added `color` struct

--- coloricious.c
+++ coloricious.c
@@ -7,23 +7,28 @@
 
 static float truncate(float);
 
-static void color8bit(float, float, float);
-static void color24bit(float, float, float);
+struct color
+{
+    float r;
+    float g;
+    float b;
+};
+
+struct color randomcolor(void);
+
+static void color8bit(struct color);
+static void color24bit(struct color);
 static void uncolor(void);
 
 int main(void)
 {
+    struct color rgb;
     int c;
 
     srand(time(NULL));
 
     while ((c = getchar()) != -1) {
-
-        float r = (float) rand() / INT_MAX;
-        float g = (float) rand() / INT_MAX;
-        float b = (float) rand() / INT_MAX;
-
-        color8bit(r, g, b);
+        color24bit(randomcolor());
         putchar(c);
     }
 
@@ -37,20 +42,29 @@ static float truncate(float f)
     return fmax(0.0, fmin(f, 1.0));
 }
 
-static void color8bit(float r, float g, float b)
+struct color randomcolor(void)
 {
-    int rc = round(6 * truncate(r));
-    int gc = round(6 * truncate(g));
-    int bc = round(6 * truncate(b));
+    struct color rgb;
+    rgb.r = (float) rand() / INT_MAX;
+    rgb.g = (float) rand() / INT_MAX;
+    rgb.b = (float) rand() / INT_MAX;
+    return rgb;
+}
+
+static void color8bit(struct color rgb)
+{
+    int rc = round(6 * truncate(rgb.r));
+    int gc = round(6 * truncate(rgb.g));
+    int bc = round(6 * truncate(rgb.b));
     int cc = (36 * rc) + (6 * gc) + bc + 16;
     printf("\e[38;5;%dm", cc);
 }
 
-static void color24bit(float r, float g, float b)
+static void color24bit(struct color rgb)
 {
-    int rc = round(255 * truncate(r));
-    int gc = round(255 * truncate(g));
-    int bc = round(255 * truncate(b));
+    int rc = round(255 * truncate(rgb.r));
+    int gc = round(255 * truncate(rgb.g));
+    int bc = round(255 * truncate(rgb.b));
     printf("\e[38;2;%d;%d;%dm", rc, gc, bc);
 }
 
